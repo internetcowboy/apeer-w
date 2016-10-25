186941d4e4c7d595f484c956df4815b60ebb8cb8
Codin Pangell <codin@internetcowboy.org>
2016-10-24 19:32:24 -0700

    text updates

--- www/assets/js/application.js
+++ www/assets/js/application.js
@@ -1,34 +1,34 @@
 
 // Use waypoints to swap the screen on the phone
 $(function() {
-	var waypoints = [
-		{ id : 'assignment', waypoint : {} },
-		{ id: 'communication', waypoint : {} },
-		{ id: 'security', waypoint : {} },
+    var waypoints = [
+        { id : 'assignment', waypoint : {} },
+        { id: 'communication', waypoint : {} },
+        { id: 'security', waypoint : {} },
         { id: 'appearance', waypoint : {} },
-		
-	];
+        
+    ];
 
-	for(var i=0; i < waypoints.length; i++) { 
+    for(var i=0; i < waypoints.length; i++) { 
 
-		var waypoint_id = waypoints[i].id
-		if(!document.getElementById(waypoint_id)) return;
+        var waypoint_id = waypoints[i].id
+        if(!document.getElementById(waypoint_id)) return;
 
-		waypoints[i].waypoint = new Waypoint({
-			element: document.getElementById(waypoint_id),
-			handler: function(direction) {
-				var name = "scroll_" + $(this.element).attr('id');
-				if(direction == "down") {
-					$("#"+name).removeClass('scroll-screen-down');
-					$("#"+name).prev().addClass('scroll-screen-up');
-				} else {
-					$("#"+name).prev().removeClass('scroll-screen-up');
-					$("#"+name).addClass('scroll-screen-down');
-				}
-			},
-			offset: 100 
-		});
-	}
+        waypoints[i].waypoint = new Waypoint({
+            element: document.getElementById(waypoint_id),
+            handler: function(direction) {
+                var name = "scroll_" + $(this.element).attr('id');
+                if(direction == "down") {
+                    $("#"+name).removeClass('scroll-screen-down');
+                    $("#"+name).prev().addClass('scroll-screen-up');
+                } else {
+                    $("#"+name).prev().removeClass('scroll-screen-up');
+                    $("#"+name).addClass('scroll-screen-down');
+                }
+            },
+            offset: 100 
+        });
+    }
 
     $('.learn-cta a.btn').on('click', function(e) {
         $(this).parent().siblings('.poster.lead').slideToggle('fast');
